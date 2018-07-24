b005d042b63cc3210fedefe690cfdebdf89d77fa
Monoboting <41525699+Monoboting@users.noreply.github.com>
2018-07-23 23:12:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,20 +3,23 @@ const client = new Discord.Client();
 
 //translator
 const fs = require("fs");
+/*
 const Eris = require("eris");
 const OS = require("os");
-/*
+*/
 const translate = require("google-translate-api")
 const lang = require("./langs.json");
 //const prefix = "m!";
 const G = require("gizoogle");
 const zalgo = require("to-zalgo");
 const flip = require("flipout");
+/*
 const kpop = require("kpop");
 const japanese = require("japanese");
 const ostb = require("os-toolbox");
-const langs = require("./langmap.json");
 */
+const langs = require("./langmap.json");
+
 
 // Get the text file and load it into a variable.
 var lbclasserc = fs.readFileSync("./lbClassic.txt", "utf8");
