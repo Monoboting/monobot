a5efa64593b016246e1a5c15a7873ccc90f407cc
Monoboting <41525699+Monoboting@users.noreply.github.com>
2018-08-23 15:43:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1113,6 +1113,9 @@ client.on('message', message => {
 	if(message.content == 'm!lapras'){
 	    message.channel.send("<a:lapras:471783111299039259>");
     }
+	if(message.content == 'go'){
+	    message.channel.send("759");
+    }
 	
 	if(message.content == 'm!rainbow'){
 	    message.channel.send({embed: {
