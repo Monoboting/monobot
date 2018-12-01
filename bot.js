75293a1c96fa1e80ec7aafeb298128d505008018
Monoboting <41525699+Monoboting@users.noreply.github.com>
2018-12-01 15:00:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3195,7 +3195,7 @@ client.on('message', message => {
 				werd+= lerters[34] + "";
 			else if (res[cher] == "0")
 				werd+= lerters[35] + "";
-			else if (res[cher] == " ")
+			else if (res[cher] == "    ")
 				werd+= "  ";
 		}
 		
